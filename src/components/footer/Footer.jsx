e531167b852c10adc6e3d4d333c44f2077d5b78d
Samuel Asiabaka <samuelasiabaka@gmail.com>
2023-06-11 15:59:13 +0100

    Added image source domain to next.config file

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,42 @@
+import Image from "next/image";
 import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div>
+    <div className={styles.container}>
       <div>&copy;{year} ZoneTech. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="facenook-icon"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="instagram-icon"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="twitter-icon"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="youtube-icon"
+        />
+      </div>
     </div>
   );
 };
